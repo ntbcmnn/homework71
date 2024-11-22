e860f7abfcbd7e0b4346cbaba44d22d8514a0775
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-22 18:01:42 +0600

    Imported all existing app reducers to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import pizzasReducer from '../store/slices/pizzasSlice.ts';
+import { cartReducer } from '../store/slices/cartSlice.ts';
+import ordersReducer from '../store/slices/ordersSlice.ts';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    pizzas: pizzasReducer,
+    cart: cartReducer,
+    orders: ordersReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
