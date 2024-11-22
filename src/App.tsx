7eb2822a174298751e6fd7807426a48c650c94e7
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-22 18:07:41 +0600

    Displayed all containers & toolbar, added navigation to switch between client & admin sides of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css';
+import Add from './containers/Add/Add.tsx';
+import { Route, Routes } from 'react-router-dom';
+import AdminDishes from './containers/AdminDishes/AdminDishes.tsx';
+import Edit from './containers/Edit/Edit.tsx';
+import Client from './containers/Client/Client.tsx';
+import Toolbar from './components/Toolbar/Toolbar.tsx';
+import AdminOrders from './containers/AdminOrders/AdminOrders.tsx';
 
 const App = () => {
-  return <></>;
+  return <>
+    <Toolbar/>
+    <div className="container m-5">
+      <Routes>
+        <Route path="/" element={<Client/>}/>
+        <Route path="/admin" element={<AdminDishes/>}/>
+        <Route path="/admin/pizzas" element={<AdminDishes/>}/>
+        <Route path="/admin/orders" element={<AdminOrders/>}/>
+        <Route path="/admin/add" element={<Add/>}/>
+        <Route path="/admin/:pizzaId/edit" element={<Edit/>}/>
+        <Route path="*" element={<h2 className="text-center">Page not found</h2>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
